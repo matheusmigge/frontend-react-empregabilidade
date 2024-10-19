2ec33f24129130b2dafe9eb66c678c27ca7617cc
Matheus Migge <matheusmigge@gmail.com>
2024-10-19 09:09:41 -0300

    🐛 fix: chamada para o home.css levava ao caminho antigo do arquivo

--- vite-project/src/pages/Home.tsx
+++ vite-project/src/pages/Home.tsx
@@ -1,22 +1,22 @@
 // import RightSide from "../components/rightSide";
 // import LeftSide from "../components/leftSide";
 // import Header from "../components/Header";
-import "../styles/home.css"
+import "../styles/components-css/header.css"
 
-function Home () {
+function Home() {
 
 
-    return(
+    return (
         <>
-        <body className="body-container">
+            <body className="body-container">
                 <section className="leftSide">
                     <h2>Lado Esquerdo</h2>
                 </section>
 
                 <section className="rightSide">
-                        <h2>Lado Direito</h2>
+                    <h2>Lado Direito</h2>
                 </section>
-        </body>
+            </body>
         </>
     )
 }
