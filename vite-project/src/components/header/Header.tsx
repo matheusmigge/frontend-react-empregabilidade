640797753154496a41bee9003c6f920f4e7529f5
CassianoChagasJr <cassianojrchagas@gmail.com>
2024-10-24 09:59:01 -0300

    Update Header.tsx

--- vite-project/src/components/header/Header.tsx
+++ vite-project/src/components/header/Header.tsx
@@ -4,22 +4,28 @@ import Notification from "./assets/bell.svg"
 import Profile from "./assets/Ellipse 1.svg"
 
 interface HeaderProps {
-  title: string;
-  className?: string;
+  titulo: string;
+  btnFilterMap?: string;
+  btnFilterList?: string;
+  icon1: string;
+  icon2: string;
 }
 
-function Header ({title, className}: HeaderProps) {
+
+
+function Header ({titulo, btnFilterMap, btnFilterList, icon1, icon2}: HeaderProps) {
 
   return(
     <>
       <header className="header-container">
         <section className="left-Header">
               <img src={Menu} alt="Menu Hamburguer" className="menu" />
-              <h2>{title}</h2>
+              <h2>{titulo}</h2>
         </section>
 
-        <section>
-          {/* Aqui fica o toggle */}
+        <section className="filterBtn">
+              <button type="button" className="buttonFilter"> <img src={icon1} alt="icone do mapa" className="iconFilter" />{btnFilterMap}</button>
+              <button type="button" className="buttonFilter"><img src={icon2} alt="" className="iconFilter" />{btnFilterList}</button>
         </section>
 
         <section className="right-Header">
