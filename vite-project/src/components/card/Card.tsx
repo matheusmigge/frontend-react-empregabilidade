942cf6f11f2c65ad53c3b66c26de486c105e00d4
CassianoChagasJr <cassianojrchagas@gmail.com>
2024-10-25 08:35:47 -0300

    💄 feat:  atualização no component Card

---
+++ vite-project/src/components/card/Card.tsx
@@ -0,0 +1,27 @@
+import "../card/card.css"
+
+interface CardProps{
+    title: string,
+    name?: string,
+    status?: string,
+    info?: string,
+    amount?: string
+    className?: string
+}
+
+function Card ({title, name, status, info,amount, className}: CardProps) {
+
+    return(
+        <>
+            <div className="card">
+                <p className={className}>{name}</p>
+                <p className={className}>{title}</p>
+                <p className={className}>{status}</p>
+                <p className={className}>{info}</p>
+                <p className={className}>{amount}</p>
+            </div>
+        </>
+    )
+}
+
+export default Card;
