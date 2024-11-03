8eb365639e855b2db150920c5cffae27581b7e4f
Daniel Santos <dan.danielfs@gmail.com>
2024-11-03 21:10:59 +0000

    ✨ feat: Criado primeira página de cadastro de candidato

--- vite-project/src/components/user-register/UserRegister.tsx
+++ vite-project/src/components/user-register/UserRegister.tsx
@@ -2,14 +2,72 @@ import "./UserRegister.css";
 import TextualButton from "../textual-button/TextualButton";
 import linkedinVetor from "../../assets/linkedinVetor.svg";
 import googleVetor from "../../assets/googleVetor.svg";
-import mailVetor from "../../assets/mailVetor.svg";
 
 function UserRegister() {
   return (
     <>
       <div className="titleContainer">
-        <h1>Conheça diversas oportunidades de emprego</h1>
+        <h1>Cadastre-se!</h1>
       </div>
+      <form className="formContainer">
+        <div className="a">
+          <div className="b"><label htmlFor="">Nome</label>
+          <input
+            type="text"
+            id="user-name"
+            name="user-name"
+            placeholder="Digite seu email"
+          /></div>
+          
+          <div className="b"><label htmlFor="">Sobrenome</label>
+          <input
+            type="text"
+            id="user-surname"
+            name="user-surname"
+            placeholder="Digite seu sobrenome"
+          /></div>
+
+          
+        </div>
+
+        <div>
+          <label htmlFor="">Email</label>
+          <input
+            type="email"
+            id="user-email"
+            name="user-email"
+            placeholder="Informe seu email"
+          />
+
+          <label htmlFor="">Telefone</label>
+          <input
+            type="tel"
+            id="user-phone"
+            name="user-phone"
+            placeholder="(00) 00000-0000"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="">Senha</label>
+          <input
+            type="password"
+            id="user-password"
+            name="user-password"
+            placeholder="Digite sua senha"
+          />
+
+          <label htmlFor="">Confirme sua senha</label>
+          <input
+            type="password"
+            id="user-password"
+            name="user-password"
+            placeholder="Digite sua senha"
+          />
+        </div>
+
+        <TextualButton text={"PRÓXIMO"} className="submit"></TextualButton>
+      </form>
       <div className="buttonContainer">
         <TextualButton
           className="externalRegistrationButton"
@@ -20,11 +78,6 @@ function UserRegister() {
           className="externalRegistrationButton"
           text="Cadastre-se com o Google"
           imageUrl={googleVetor}
-        />
-        <TextualButton
-          className="emailButton"
-          text="Cadastre-se com o email"
-          imageUrl={mailVetor}
         />
       </div>
       <div className="loginContainer">
