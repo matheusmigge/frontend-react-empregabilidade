0d6d2c7d2abfdd8f502eee9d5b89fabbea9a2e0a
Daniel Santos <dan.danielfs@gmail.com>
2024-11-03 21:08:43 +0000

    ♻️ refactor: UserLogin ajustado

--- vite-project/src/components/user-login/UserLogin.tsx
+++ vite-project/src/components/user-login/UserLogin.tsx
@@ -1,48 +1,57 @@
 import TextualButton from "../textual-button/TextualButton";
 import linkedinVetor from "../../assets/linkedinVetor.svg";
 import googleVetor from "../../assets/googleVetor.svg";
-import "./UserLogin.css"
+import "./UserLogin.css";
 
 function UserLogin() {
-    return(
-        <>
-            <div className="user-login">
-            <div className="titleContainer">
-                <h1>Conheça diversas oportunidades de emprego</h1>
-            </div>
-            <form>
+  return (
+    <>
+      <div className="user-login">
+        <div className="titleContainer">
+          <h1>Conheça diversas oportunidades de emprego</h1>
+        </div>
+        <form>
+          <label htmlFor="user-email">Email</label>
+          <input
+            type="email"
+            id="user-email"
+            name="user-email"
+            placeholder="Digite seu email"
+          />
 
-                <label htmlFor="user-email">Email</label>
-                <input type="email" id="user-email" name="user-email" placeholder="Digite seu email"/>
+          <label htmlFor="user-password">Senha</label>
+          <input
+            type="password"
+            id="user-password"
+            name="user-password"
+            placeholder="Digite sua senha"
+          />
 
-                <label htmlFor="user-password">Senha</label>
-                <input type="password" id="user-password" name="user-password" placeholder="Digite sua senha"/>
+          <TextualButton text={"ENTRAR"} className="submit"></TextualButton>
+        </form>
 
-                <TextualButton text={'ENTRAR'} className='submit'></TextualButton>
-                
-                <hr />
+        <hr />
 
-                <div className="buttonContainer">
-                    <TextualButton
-                    className="externalRegistrationButton"
-                    text="Cadastre-se com LinkedIn"
-                    imageUrl={linkedinVetor}
-                    />
-                    <TextualButton
-                    className="externalRegistrationButton"
-                    text="Cadastre-se com Google"
-                    imageUrl={googleVetor}
-                    />
-                    <div className="signup-now">
-                        <p>
-                        Não possui uma conta? <a href="">Cadastre-se</a>
-                        </p>
-                    </div>
-                </div>
-            </form>
-            </div>
-        </>
-    )
+        <div className="buttonContainer">
+          <TextualButton
+            className="externalRegistrationButton"
+            text="Cadastre-se com LinkedIn"
+            imageUrl={linkedinVetor}
+          />
+          <TextualButton
+            className="externalRegistrationButton"
+            text="Cadastre-se com Google"
+            imageUrl={googleVetor}
+          />
+          <div className="signup-now">
+            <p>
+              Não possui uma conta? <a href="">Cadastre-se</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default UserLogin;
