b848fac5d64cf9279e7f52ac5c7f2e5d5d313c8a
CassianoChagasJr <cassianojrchagas@gmail.com>
2024-11-04 10:09:55 -0300

    💄feat: atualização no CSS do Toggle

--- vite-project/src/components/toggle/toggle.tsx
+++ vite-project/src/components/toggle/toggle.tsx
@@ -3,16 +3,26 @@ import "../toggle/toggle.css";
 interface ToggleProps {
   text1: string;
   text2: string;
+  img1: string;
+  img2: string;
 }
 
-function Toggle({ text1, text2}: ToggleProps) {
+function Toggle({ text1, text2,img1,img2 }: ToggleProps) {
   return (
     <>
       <label className="switch">
         <input type="checkbox" className="inputToggle" />
         <span className="slider"></span>
-        <div className="text text-left">{text1}</div>
-        <div className="text text-right">{text2}</div>
+        <div className="text">
+          <div className="text-left">
+            <img src={img1} alt="icone do mapa" className="iconMap" />
+            {text1}
+          </div>
+          <div className="text text-right">
+            {text2}
+            <img src={img2} alt="icone da lista" className="iconList" />
+          </div>
+        </div>
       </label>
     </>
   );
