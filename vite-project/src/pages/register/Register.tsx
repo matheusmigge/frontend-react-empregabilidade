354f61b732cba736575abcf64b656892bc385629
Daniel Santos <dan.danielfs@gmail.com>
2024-11-13 21:15:25 -0300

    💄 feat: Estilização das páginas de login do candidato

--- vite-project/src/pages/register/Register.tsx
+++ vite-project/src/pages/register/Register.tsx
@@ -3,8 +3,13 @@ import logoCompletaVetor from "../../assets/logoCompletaVetor.svg";
 import maletaVetor from "../../assets/maletaVetor.svg";
 import usuarioVetor from "../../assets/usuarioVetor.svg";
 import TextualButton from "../../components/textual-button/TextualButton";
+<<<<<<< Updated upstream
 import CompanyRegister from "../../components/company-register/CompanyRegister";
 import UserRegister from "../../components/user-register/UserRegister";
+=======
+import CompanyRegister1 from "../../components/company-register/CompanyRegister1";
+import UserRegister1 from "../../components/user-register/UserRegister1";
+>>>>>>> Stashed changes
 import { useState } from "react";
 
 function Register() {
@@ -39,8 +44,13 @@ function Register() {
         </section>
 
         <section className="rightSide">
+<<<<<<< Updated upstream
           {registerType == "company" && <CompanyRegister></CompanyRegister>}
           {registerType == "candidate" && <UserRegister></UserRegister>}
+=======
+          {registerType == "company" && <CompanyRegister1></CompanyRegister1>}
+          {registerType == "candidate" && <UserRegister1></UserRegister1>}
+>>>>>>> Stashed changes
         </section>
 
       </body>
