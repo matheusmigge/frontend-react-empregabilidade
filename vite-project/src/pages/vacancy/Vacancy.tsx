808056e9114fb718362266c493c5bc628c7c3658
Daniel Santos <dan.danielfs@gmail.com>
2024-11-14 22:36:09 -0300

    ✨ feat: Criada a página de vaga

---
+++ vite-project/src/pages/vacancy/Vacancy.tsx
@@ -0,0 +1,101 @@
+import "./Vacancy.css";
+import AccordionContainer from "./accordionContainer/AccordionBox";
+import FotoDeTeste from "./FOTODETESTE.png";
+
+function Vacancy() {
+  return (
+    <>
+      <div className="vacancyContainer">
+        <div className="headerTeste"></div>
+
+        <div className="vacancyContent">
+          <div className="vacancyBannerContainer">
+            <img src={FotoDeTeste} alt="" />
+          </div>
+
+          <AccordionContainer title="Descrição da Vaga">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
+              quaerat fuga nam quasi ea earum quia provident nihil,
+              reprehenderit quidem at nesciunt velit exercitationem sit? Iust o
+              esse aperiam earum aspernatur. Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Autem labore, recusandae hic
+              eligendi blanditiis quo nulla aut rem quasi! Officia ullam quos
+              totam esse rem velit eum possimus fugiat nesciunt! Lorem ipsum,
+              dolor sit amet consectetur adipisicing elit. Incidunt nulla ipsa
+              debitis dolore in voluptatibus perferendis facilis eaque
+              architecto ducimus sequi inventore cum excepturi laudantium,
+              tempore, quo eos molestiae! Eligendi. Lorem ipsum, dolor sit amet
+              consectetur adipisicing elit. Harum repudiandae exercitationem
+              vel? Illum officia deserunt dolor, corporis necessitatibus aut
+              quia repudiandae, sit suscipit dolorem eum impedit ullam nostrum
+              quod debitis!
+            </p>
+          </AccordionContainer>
+
+          <AccordionContainer title="Responsabilidades e atribuições">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
+              quaerat fuga nam quasi ea earum quia provident nihil,
+              reprehenderit quidem at nesciunt velit exercitationem sit? Iusto
+              esse aperiam earum aspernatur. Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Autem labore, recusandae hic
+              eligendi blanditiis quo nulla aut rem quasi! Officia ullam quos
+              totam esse rem velit eum possimus fugiat nesciunt! Lorem ipsum,
+              dolor sit amet consectetur adipisicing elit. Incidunt nulla ipsa
+              debitis dolore in voluptatibus perferendis facilis eaque
+              architecto ducimus sequi inventore cum excepturi laudantium,
+              tempore, quo eos molestiae! Eligendi. Lorem ipsum, dolor sit amet
+              consectetur adipisicing elit. Harum repudiandae exercitationem
+              vel? Illum officia deserunt dolor, corporis necessitatibus aut
+              quia repudiandae, sit suscipit dolorem eum impedit ullam nostrum
+              quod debitis!
+            </p>
+          </AccordionContainer>
+
+          <AccordionContainer title="Requisitos e qualificações">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
+              quaerat fuga nam quasi ea earum quia provident nihil,
+              reprehenderit quidem at nesciunt velit exercitationem sit? Iusto
+              esse aperiam earum aspernatur. Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Autem labore, recusandae hic
+              eligendi blanditiis quo nulla aut rem quasi! Officia ullam quos
+              totam esse rem velit eum possimus fugiat nesciunt! Lorem ipsum,
+              dolor sit amet consectetur adipisicing elit. Incidunt nulla ipsa
+              debitis dolore in voluptatibus perferendis facilis eaque
+              architecto ducimus sequi inventore cum excepturi laudantium,
+              tempore, quo eos molestiae! Eligendi. Lorem ipsum, dolor sit amet
+              consectetur adipisicing elit. Harum repudiandae exercitationem
+              vel? Illum officia deserunt dolor, corporis necessitatibus aut
+              quia repudiandae, sit suscipit dolorem eum impedit ullam nostrum
+              quod debitis!
+            </p>
+          </AccordionContainer>
+
+          <AccordionContainer title="O que aumenta as suas chances?">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
+              quaerat fuga nam quasi ea earum quia provident nihil,
+              reprehenderit quidem at nesciunt velit exercitationem sit? Iusto
+              esse aperiam earum aspernatur. Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Autem labore, recusandae hic
+              eligendi blanditiis quo nulla aut rem quasi! Officia ullam quos
+              totam esse rem velit eum possimus fugiat nesciunt! Lorem ipsum,
+              dolor sit amet consectetur adipisicing elit. Incidunt nulla ipsa
+              debitis dolore in voluptatibus perferendis facilis eaque
+              architecto ducimus sequi inventore cum excepturi laudantium,
+              tempore, quo eos molestiae! Eligendi. Lorem ipsum, dolor sit amet
+              consectetur adipisicing elit. Harum repudiandae exercitationem
+              vel? Illum officia deserunt dolor, corporis necessitatibus aut
+              quia repudiandae, sit suscipit dolorem eum impedit ullam nostrum
+              quod debitis!
+            </p>
+          </AccordionContainer>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Vacancy;
---
+++ vite-project/src/pages/vacancy/accordionContainer/AccordionBox.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './AccordionBox.css';
+import arrowVector from '../../../assets/arrowVector.svg';
+
+interface AccordionProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const AccordionBox = ({ title, children }: AccordionProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen(prevState => !prevState);
+  };
+
+  return (
+    <div className="accordionContainer">
+      <div className={`accordionHeader ${isOpen ? "open" : "close"}`} onClick={toggleAccordion}>
+      <img src={arrowVector} alt="Toggle" style={{ transform: isOpen ? 'rotate(-180deg)' : 'rotate(0deg)' }} />
+        {title}
+      </div>
+      {isOpen && <div className="accordionContent">{children}</div>}
+    </div>
+  );
+};
+
+export default AccordionBox;
