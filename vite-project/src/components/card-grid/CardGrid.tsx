00e45368e09ff48de582ecac50615d66c1d3f78a
CassianoChagasJr <cassianojrchagas@gmail.com>
2024-11-15 22:28:57 -0300

    💄 feat: modificações no CSS do grid de card
    adição do scroll interno para se enquadrar ao modelo do protótipo feito no figma

--- vite-project/src/components/card-grid/CardGrid.tsx
+++ vite-project/src/components/card-grid/CardGrid.tsx
@@ -1,21 +1,113 @@
-import "./CardGrid.css"
+import "./CardGrid.css";
+import Card from "../card/Card";
+import Logo from "../card/assets/Logo.svg";
 
 function CardGrid() {
   return (
     <>
-      <div className="grid-container">
-        <div className="grid-item">1</div>
-        <div className="grid-item">2</div>
-        <div className="grid-item">3</div>
-        <div className="grid-item">4</div>
-        <div className="grid-item">5</div>
-        <div className="grid-item">6</div>
-        <div className="grid-item">7</div>
-        <div className="grid-item">8</div>
-        <div className="grid-item">9</div>
+      <div className="externalContainer">
+        <div className="grid-container">
+          <Card
+            companyName="TecNorte"
+            logoName={Logo}
+            available
+            jobTitle="Desenvolvedor Front-End com React"
+            info="Modelo de trabalho: Hibrido"
+            amount="50"
+          />
+          <Card
+            companyName="TecNorte"
+            logoName={Logo}
+            available
+            jobTitle="Desenvolvedor Front-End com React"
+            info="Modelo de trabalho: Hibrido"
+            amount="50/100"
+          />
+          <Card
+            companyName="TecNorte"
+            logoName={Logo}
+            available
+            jobTitle="Desenvolvedor Front-End com React"
+            info="Modelo de trabalho: Hibrido"
+            amount="50"
+          />
+          <Card
+            companyName="TecNorte"
+            logoName={Logo}
+            available
+            jobTitle="Desenvolvedor Front-End com React"
+            info="Modelo de trabalho: Hibrido"
+            amount="50"
+          />
+          <Card
+            companyName="TecNorte"
+            logoName={Logo}
+            available
+            jobTitle="Desenvolvedor Front-End com React"
+            info="Modelo de trabalho: Hibrido"
+            amount="50"
+          />
+          <Card
+            companyName="TecNorte"
+            logoName={Logo}
+            available
+            jobTitle="Desenvolvedor Front-End com React"
+            info="Modelo de trabalho: Hibrido"
+            amount="50"
+          />
+          <Card
+            companyName="TecNorte"
+            logoName={Logo}
+            available
+            jobTitle="Desenvolvedor Front-End com React"
+            info="Modelo de trabalho: Hibrido"
+            amount="50"
+          />
+          <Card
+            companyName="TecNorte"
+            logoName={Logo}
+            available
+            jobTitle="Desenvolvedor Front-End com React"
+            info="Modelo de trabalho: Hibrido"
+            amount="50"
+          />
+          <Card
+            companyName="TecNorte"
+            logoName={Logo}
+            available
+            jobTitle="Desenvolvedor Front-End com React"
+            info="Modelo de trabalho: Hibrido"
+            amount="50"
+          />
+          <Card
+            companyName="TecNorte"
+            logoName={Logo}
+            available
+            jobTitle="Desenvolvedor Front-End com React"
+            info="Modelo de trabalho: Hibrido"
+            amount="50"
+          />
+          <Card
+            companyName="TecNorte"
+            logoName={Logo}
+            available
+            jobTitle="Desenvolvedor Front-End com React"
+            info="Modelo de trabalho: Hibrido"
+            amount="50"
+          />
+          <Card
+            companyName="TecNorte"
+            logoName={Logo}
+            available
+            jobTitle="Desenvolvedor Front-End com React"
+            info="Modelo de trabalho: Hibrido"
+            amount="50"
+          />         
+        </div>
       </div>
     </>
   );
 }
 
 export default CardGrid;
+
