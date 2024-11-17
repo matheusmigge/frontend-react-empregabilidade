1462d0625882b848c189fc84f26f4665c6b9fb7b
Matheus Migge <matheusmigge@gmail.com>
2024-11-17 00:52:02 -0300

    ✨ feat: Leaflet implementado com sucesso

--- vite-project/src/pages/home/Home.tsx
+++ vite-project/src/pages/home/Home.tsx
@@ -1,10 +1,14 @@
-import FiltersBar from "../../components/filters-bar/FiltersBar"
-import Header from "../../components/header/Header"
-import "./Home.css"
+import "./Home.css";
+import "leaflet/dist/leaflet.css"
 
-import menuIcon from "../../components/header/assets/Menu.svg" ;
-import bellIcon from "../../components/header/assets/bell.svg"
-import userIcon from "../../components/header/assets/Ellipse 1.svg"
+import FiltersBar from "../../components/filters-bar/FiltersBar";
+import Header from "../../components/header/Header";
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
+import menuIcon from "../../components/header/assets/Menu.svg";
+import bellIcon from "../../components/header/assets/bell.svg";
+import userIcon from "../../components/header/assets/Ellipse 1.svg";
+
 
 function Home() {
     return (
@@ -15,7 +19,17 @@ function Home() {
                 <FiltersBar></FiltersBar>
 
                 <div className="content">
-                    <p>Conteúdo rolável da página</p>
+                    <MapContainer center={[-8.0618064,-34.871831,21]} zoom={18} scrollWheelZoom={false}>
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[-8.0618064,-34.871831,21]}>
+                            <Popup>
+                                Pernambuco trabalhando 🚀
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
                 </div>
             </div>
         </div>
