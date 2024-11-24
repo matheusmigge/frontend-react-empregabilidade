da15e8ed84b212ee26c8915706821e5c6a68b762
CassianoChagasJr <cassianojrchagas@gmail.com>
2024-11-23 22:39:44 -0300

    ✨ feat: atualização no app.tsx

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,12 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import LandingPage from "./pages/landingPage/LandingPage";
 import Entrance from "./pages/entrance/Entrance";
 import "./App.css";
-import Home from "./pages/home/Home";
 
 function App() {
   return (
     <>
-    <Home></Home>
       <Router>
         <Routes>
           {/* <Route path="/" element={<LandingPage />} /> */}
