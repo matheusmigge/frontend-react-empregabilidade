82f1379cc65c1cbc1f0166001b112c66e549f3f6
DouglasHennrik <douglashennrik@gmail.com>
2025-04-15 23:42:05 -0300

    ✨ feat: Adição do Header na página de currículo

--- vite-project/src/pages/entrance/user-curriculum/userCurriculum.tsx
+++ vite-project/src/pages/entrance/user-curriculum/userCurriculum.tsx
@@ -1,8 +1,29 @@
+import "./userCurriculum.css";
+
+import Header from "../../../components/header/Header";
+import bellIcon from "../../../components/header/assets/bell.svg";
+import userIcon from "../../../components/header/assets/Ellipse 1.svg";
+import goBackVector from "../../vacancy/assets/goBackVector.svg";
+import { Link } from "react-router-dom";
+
+
+
+
 
 function UserCurriculum() {
     return (
       <>
-       <h1>....</h1>
+      <div>
+
+      <Link to="/userSignUp2" className="linkStyle">
+       <Header
+        title="Editar Perfil"
+        imgUrl1={bellIcon}
+        imgUrl2={userIcon}
+        imgUrl={goBackVector}
+        ></Header>;
+    </Link>
+      </div>
       </>
     );
   }
