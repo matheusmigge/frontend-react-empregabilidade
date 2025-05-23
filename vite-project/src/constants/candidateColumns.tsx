73c344af1fdf67dc222365b494a5778df0ca4427
Matheus Migge <matheusmigge@gmail.com>
2025-05-22 21:36:56 -0300

    ✨ feat: Configurado as tabelas de candidatos e vagas com filtros e icones

---
+++ vite-project/src/constants/candidateColumns.tsx
@@ -0,0 +1,24 @@
+import nameIcon from "../assets/candidates-icons/description.svg";
+import distanceIcon from "../assets/candidates-icons/map-pin.svg";
+import skillsIcon from "../assets/candidates-icons/icon_pricetags.svg";
+import compatibilityIcon from "../assets/candidates-icons/icon-park-outline_user-to-user-transmission.svg";
+import applicationPhaseIcon from "../assets/candidates-icons/icon_business-progress.svg";
+import higherEducationIcon from "../assets/candidates-icons/book.svg";
+import certificationsIcon from "../assets/candidates-icons/award.svg";
+import formerExperiencesIcon from "../assets/candidates-icons/trending-up.svg";
+import { FilterButtonProps } from "../components/filter-button/FilterButton"
+
+const CANDIDATE_COLUMN: Pick<FilterButtonProps, 'iconUrl' | 'text'>[] = [
+
+    { iconUrl: nameIcon, text: "Nome" },
+    { iconUrl: distanceIcon, text: "Distância" },
+    { iconUrl: skillsIcon, text: "Habilidades e competências" },
+    { iconUrl: compatibilityIcon, text: "Compatibilidade" },
+    { iconUrl: applicationPhaseIcon, text: "Fase de Aplicação" },
+    { iconUrl: higherEducationIcon, text: "Formação" },
+    { iconUrl: certificationsIcon, text: "Certificações" },
+    { iconUrl: formerExperiencesIcon, text: "Experiências" },
+
+];
+
+export default CANDIDATE_COLUMN;
