ce4d4526536984bddc040e8f0671ea2dd986afc1
ViniciusSL12 <viniciussimaslima19@.com>
2025-05-29 22:25:57 -0300

    ✨: desfazendo o último commit

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,8 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Entrance from "./pages/entrance/Entrance";
 import LandingPage from "./pages/landingPage/LandingPage";
 import "./App.css";
-import UserSignUp from "./pages/entrance/user-signUp/UserSignUp1";
-import CompanySignUp from "./pages/entrance/company-signUp/CompanySignUp1";
+import UserSignUp1 from "./pages/entrance/user-signUp/UserSignUp1";
+import UserSignUp2 from "./pages/entrance/user-signUp/UserSignUp2";
+import CompanySignUp1 from "./pages/entrance/company-signUp/CompanySignUp1";
+import CompanySignUp2 from "./pages/entrance/company-signUp/CompanySignUp2";
 import Home from "./pages/home/Home";
 import Vacancy from "./pages/vacancy/Vacancy";
 
@@ -12,17 +14,17 @@ function App() {
     <>
       <Router>
         <Routes>
-        <Route path="/" element={<LandingPage/>}/>
-          <Route path="/entrance" element={<Entrance/>}/>
-          <Route path="/userSignUp" element={<UserSignUp/>}/>
-          <Route path="/companySignUp" element={<CompanySignUp/>}/>
-          <Route path="/home" element={<Home/>} />
-          <Route path="/vacancy" element={<Vacancy/>}/>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/entrance" element={<Entrance />} />
+          <Route path="/userSignUp1" element={<UserSignUp1 />} />
+          <Route path="/userSignUp2" element={<UserSignUp2 />} />
+          <Route path="/companySignUp1" element={<CompanySignUp1 />} />
+          <Route path="/companySignUp2" element={<CompanySignUp2 />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/vacancy" element={<Vacancy />} />
         </Routes>
       </Router>
     </>
   );
 }
-
-export default App;
-
+export default App; 
